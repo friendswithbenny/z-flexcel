f3a2615128df8f7f06a88e6dbcb7799feb784b85
Benny <friendswithbenny@gmail.com>
2016-02-13 20:10:58 -0800

    opencsv
    added opencsv example for sql2csv.

---
+++ src/main/java/org/fwb/flexcel/csv/Sql2Csv.java
@@ -0,0 +1,24 @@
+package org.fwb.flexcel.csv;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+public class Sql2Csv {
+	/** @deprecated static utilities only */
+	@Deprecated
+	private Sql2Csv() { }
+	
+	public static void rs2csv(
+			ResultSet rs, Writer w, boolean header)
+			throws SQLException, IOException {
+		CSVWriter csv = new CSVWriter(w); try {
+			csv.writeAll(rs, header);
+		} finally {
+			csv.close();
+		}
+	}
+}
